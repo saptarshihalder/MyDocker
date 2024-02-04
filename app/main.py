633fb35a3b39e94d41ac28345a07c610a9df7c4f
Saptarshi Halder <62690091+saptarshihalder@users.noreply.github.com>
2024-02-05 00:25:31 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,19 +2,21 @@ import subprocess
 import sys
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # Extract the command and arguments from the command line arguments
     command = sys.argv[3]
     args = sys.argv[4:]
-    # Uncomment this block to pass the first stage
-    #
-    # command = sys.argv[3]
-    # args = sys.argv[4:]
-    #
-    # completed_process = subprocess.run([command, *args], capture_output=True)
-    # print(completed_process.stdout.decode("utf-8"))
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+
+    # Create a subprocess that connects the stdout and stderr to the respective streams of the parent process
+    completed_process = subprocess.Popen(
+        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    # Communicate with the process to read the stdout and stderr, then write it to the parent process' streams
+    stdout, stderr = completed_process.communicate()
+    sys.stdout.buffer.write(stdout)
+    sys.stdout.buffer.flush()
+    sys.stderr.buffer.write(stderr)
+    sys.stderr.buffer.flush()
 
 if __name__ == "__main__":
     main()

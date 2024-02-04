37ddb67be1db568ecc45ab8ab6a4e833c674bde4
Saptarshi Halder <62690091+saptarshihalder@users.noreply.github.com>
2024-02-05 00:08:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 import subprocess
 import sys
 
-
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
+    command = sys.argv[3]
+    args = sys.argv[4:]
     # Uncomment this block to pass the first stage
     #
     # command = sys.argv[3]
@@ -13,7 +13,8 @@ def main():
     #
     # completed_process = subprocess.run([command, *args], capture_output=True)
     # print(completed_process.stdout.decode("utf-8"))
-
+    completed_process = subprocess.run([command, *args], capture_output=True)
+    print(completed_process.stdout.decode("utf-8"))
 
 if __name__ == "__main__":
     main()
